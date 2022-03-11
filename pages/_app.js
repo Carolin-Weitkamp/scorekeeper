ee13ed337ad6b096c7313eb66a554ba04d865a6d
Carolin Weitkamp <c.weitkamp@posteo.de>
2022-03-11 15:20:25 +0100

    change history entry

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function MyApp({ Component, pageProps }) {
   const [players, setPlayers] = useState([]);
   const [nameOfGame, setNameOfGame] = useState("");
+  console.log(nanoid());
   return <Component {...pageProps} />;
 }
 
