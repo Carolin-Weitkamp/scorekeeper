b3e4a90e789fa85fedd8c12cb41625ecb3ee1c9c
Florian Hippenstiel <florian.hippenstiel@web.de>
2022-03-11 12:01:15 +0100

    added form field

--- components/GameForm.js
+++ components/GameForm.js
@@ -1,27 +1,40 @@
 import styled from "styled-components";
 import { useState } from "react";
-import InputComponent from "./InputComponent"
+import InputComponent from "./InputComponent";
 
-export default function GameForm () {
-    return (
-        <>
-        <h1>Scorekeeper</h1>
-        <StyledForm
-            aria-labelledby="formHeader"
-            // onSubmit={handleSubmit}
-            autoComplete="off"
-        >
+export default function GameForm() {
+  return (
+    <>
+      <h1>Scorekeeper</h1>
+      <StyledForm
+        aria-labelledby="formHeader"
+        // onSubmit={handleSubmit}
+        autoComplete="off"
+      >
         <h2 id="formHeader">New Game</h2>
-        <InputComponent />
-
-
-        </StyledForm>
-        </>
-    )
+        <InputComponent
+          name="nameOfGame"
+          labelText="Name of game"
+          placeholder="e.g. Scrabble"
+          //   onChange={handleChange}
+          //   value={formData.nameOfGame}
+          required
+        />
+        <InputComponent
+          name="playerNames"
+          labelText="Player names, seperated by comma"
+          placeholder="e.g. Johny, Janette"
+          //   onChange={handleChange}
+          //   value={formData.playerNames}
+          required
+        />
+      </StyledForm>
+    </>
+  );
 }
 
 const StyledForm = styled.form`
-    display: grid;
-    gap: 10px;
-    background-color: hotpink;
+  display: grid;
+  gap: 10px;
+  background-color: hotpink;
 `;
